c36c93d4d7297a7ee66f09d146cb804da4350d1c
Austin Gibbons <austingibb@gmail.com>
2017-11-26 00:57:46 -0700

    Added game loop, lerping, and math libraries to npm dependencies

---
+++ Player.js
@@ -0,0 +1,28 @@
+﻿var lerp = require('lerp');
+
+module.exports = function () {
+    return new Player();
+};
+
+function Player() {
+    this.Update = Update;
+    this.MoveTo = MoveTo;
+    this._dotMode = false;
+    this.GetRadius = GetRadius;
+}
+
+function MoveTo(x, y) {
+    if (this._dotMode == true) {
+
+    }
+}
+
+function Update(delta) {
+
+}
+
+function GetRadius() {
+
+}
+
+
---
+++ app.js
@@ -0,0 +1,4 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var socketio = require('socket.io')(http);
+
